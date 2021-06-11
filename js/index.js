6ad62997630cd4564cc7e4e10c70d4fdbe6d1eb4
ChyoRi <chyori_@naver.com>
2021-06-11 21:00:36 +0900

    첫 커밋

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+var SELECT = 0;
+
+$(function(){
+
+    $('button').click(function(){
+        var i = $(this).parents('tr').index();
+        SELECT = i;
+        $('.modal').toggle();
+    });
+
+    $('.modal .remove').click(function(event){
+        event.preventDefault();
+        $('tbody > tr').eq(SELECT).hide();
+        console.log(SELECT);
+        $('.modal').hide();
+
+    });
+
+});
